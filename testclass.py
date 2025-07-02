ca846ccc8eda3002f7745ff5bb0ff156d11aab80
Z3R0Galaxy <orlandogien@icloud.com>
2025-07-02 15:27:35 +1000

    Added support of mutiple names

--- testclass.py
+++ testclass.py
@@ -1,11 +1,15 @@
 class Me:
-    def __init__(self, favColor: str, favNum: int, sport: str):
+    def __init__(self, name: str, favColor: str, favNum: int, sport: str):
+        self.name = name
         self.favColor = favColor
         self.favNum = favNum
         self.sport = sport
 
     def describe(self):
-        print(f"my fav color is {self.favColor}")
+        print(f"{self.name}'s favorite color is {self.favColor}, favorite number is {self.favNum}, and sport is {self.sport}.")
 
-person = Me("blue", 2, "volleyball")
-person.describe()
+Orlando = Me("Orlando", "blue", 2, "volleyball")
+Alex = Me("Alex", "red", 6, "F1")
+
+Orlando.describe()
+Alex.describe()
